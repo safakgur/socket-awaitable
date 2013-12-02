f861b68433882fa519eb7f2036cba1af40e28de8
Şafak Gür <safak9ur@gmail.com>
2013-12-03 00:51:25 +0200

    Add unit tests for SocketEx.
    Fix SocketAwaiter class to pass all the tests.

---
+++ src/Dawn.SocketAwaitable.Tests/SocketExTests.cs
@@ -0,0 +1,99 @@
+﻿// Copyright
+// ----------------------------------------------------------------------------------------------------------
+//  <copyright file="SocketExTests.cs" company="https://github.com/safakgur/Dawn.SocketAwaitable">
+//      MIT
+//  </copyright>
+//  <license>
+//      This source code is subject to terms and conditions of The MIT License (MIT).
+//      A copy of the license can be found in the License.txt file at the root of this distribution.
+//  </license>
+//  <summary>
+//      Provides a class that contains unit tests for awaitable socket extensions.
+//  </summary>
+// ----------------------------------------------------------------------------------------------------------
+
+namespace Dawn.Net.Sockets.Tests
+{
+    using System;
+    using System.Net;
+    using System.Net.Sockets;
+    using System.Threading.Tasks;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    /// <summary>
+    ///     Contains unit tests for <see cref="SocketEx" /> class.
+    /// </summary>
+    [TestClass]
+    public class SocketExTests
+    {
+        #region Methods
+        /// <summary>
+        ///     Tests <see cref="SocketEx.AcceptAsync" />, <see cref="SocketEx.ConnectAsync" />,
+        ///     <see cref="SocketEx.ReceiveAsync" /> and <see cref="SocketEx.SendAsync" /> methods.
+        /// </summary>
+        /// <returns>
+        ///     A task that represents the asynchronous testing operation.
+        /// </returns>
+        [TestMethod]
+        public async Task TestCommonOperations()
+        {
+            // Listen.
+            using (var listener = new Socket(SocketType.Stream, ProtocolType.Tcp))
+            {
+                listener.Bind(new IPEndPoint(IPAddress.IPv6Any, 0));
+                listener.Listen(1);
+                var acceptReceiveTask = Task.Run(async () =>
+                {
+                    // Accept.
+                    Socket accepted;
+                    using (var acceptAwaitable = new SocketAwaitable())
+                    {
+                        Assert.IsNull(acceptAwaitable.AcceptSocket);
+
+                        var acceptResult = await listener.AcceptAsync(acceptAwaitable);
+                        Assert.AreEqual(acceptResult, SocketError.Success);
+                        Assert.IsNotNull(acceptAwaitable.AcceptSocket);
+                        accepted = acceptAwaitable.AcceptSocket;
+                    }
+
+                    // Receive.
+                    using (var receiveAwaitable = new SocketAwaitable())
+                    {
+                        receiveAwaitable.Buffer = new ArraySegment<byte>(new byte[16], 2, 14);
+
+                        var receiveResult = await accepted.ReceiveAsync(receiveAwaitable);
+                        Assert.AreEqual(receiveResult, SocketError.Success);
+                        Assert.AreEqual(receiveAwaitable.Transferred.Count, 1);
+                        Assert.AreEqual(receiveAwaitable.Buffer.Array[receiveAwaitable.Buffer.Offset], 7);
+                    }
+                });
+
+                // Connect.
+                using (var client = new Socket(SocketType.Stream, ProtocolType.Tcp))
+                {
+                    using (var connectAwaitable = new SocketAwaitable())
+                    {
+                        connectAwaitable.RemoteEndPoint = new IPEndPoint(IPAddress.IPv6Loopback, (listener.LocalEndPoint as IPEndPoint).Port);
+
+                        var connectResult = await client.ConnectAsync(connectAwaitable);
+                        Assert.AreEqual(connectResult, SocketError.Success);
+                    }
+
+                    // Send.
+                    using (var sendAwaitable = new SocketAwaitable())
+                    {
+                        sendAwaitable.Buffer = new ArraySegment<byte>(new byte[] { 0, 1, 2, 3, 4, 5, 6, 7 }, 7, 1);
+
+                        var sendResult = await client.SendAsync(sendAwaitable);
+                        Assert.AreEqual(sendResult, SocketError.Success);
+                    }
+
+                    // Await accept/receive task.
+                    await acceptReceiveTask;
+                    Assert.AreEqual(acceptReceiveTask.Status, TaskStatus.RanToCompletion);
+                }
+            }
+        }
+        #endregion
+    }
+}
