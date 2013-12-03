416c6853af46308744bf5fee053468c7a8562ce2
Şafak Gür <s+github@gur.uk>
2013-12-03 02:30:24 +0200

    Increase assembly version to 1.0.

--- src/Dawn.SocketAwaitable/Properties/AssemblyInfo.cs
+++ src/Dawn.SocketAwaitable/Properties/AssemblyInfo.cs
@@ -24,8 +24,8 @@ using System.Runtime.InteropServices;
 
 [assembly: CLSCompliant(true)]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
